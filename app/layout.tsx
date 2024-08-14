40fc2f82015bf97f2ef3239e64237586367ca669
wachirawitPPP <wachirawit.works@gmail.com>
2024-08-14 11:44:47 +0700

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,16 +9,17 @@ import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+// export const metadata: Metadata = {
+  
+//   title: {
+//     default: siteConfig.name,
+//     template: `%s - ${siteConfig.name}`,
+//   },
+//   description: siteConfig.description,
+//   icons: {
+//     icon: "/favicon.ico",
+//   },
+// };
 
 export const viewport: Viewport = {
   themeColor: [
